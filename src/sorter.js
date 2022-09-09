6aaf4c85dd288886383e66990a018960595fe486
Jakub Záruba <jakub.zaruba@oorgis.cz>
2022-09-09 16:28:56 +0200

    [IMP] WIP

--- src/sorter.js
+++ src/sorter.js
@@ -14,68 +14,84 @@ const input = [
 ];
 
 // const input = [
-// 	'background: #f3f3f3;',
-// 	'border: 1px solid #e4e4e4;',
-// 	'border-radius: 5px 5px 0 0;',
-// 	'cursor: pointer;',
-// 	'flex: 1 1 auto;',
+// 'background: #f3f3f3;',
+// 'border-radius: 5px 5px 0 0;',
+// 'border: 1px solid #e4e4e4;',
+// 'cursor: pointer;',
+// 'flex: 1 1 auto;',
 // ];
 
-sortLines = function (lines) {
-	lines.sort(function (a, b) {
-		a = a.toString();
-		b = b.toString();
+const superSort = function (lineA, lineB) {
+	lineA = lineA.toString();
+	lineB = lineB.toString();
 
-		for (
-			var index = 0, maxLength = Math.max(a.length, b.length);
-			index < maxLength && a.charAt(index) === b.charAt(index);
-			++index
-		);
+	for (
+		var index = 0, maxLength = Math.max(lineA.length, lineB.length);
+		index < maxLength && lineA.charAt(index) === lineB.charAt(index);
+		++index
+	);
 
-		if (index === maxLength) {
-			return 0;
+	if (index === maxLength) {
+		return 0;
+	}
+
+	const
+		charOne = lineA.charAt(index),
+		charTwo = lineB.charAt(index),
+		charOneLowerCase = charOne.toLowerCase(),
+		charTwoLowerCase = charTwo.toLowerCase();
+
+	function checkExceptions(valueA, valueB) {
+		if (valueA === '-') {
+			return 1;
 		}
 
-		const
-			charOne = a.charAt(index),
-			charTwo = b.charAt(index),
-			charOneLowerCase = charOne.toLowerCase(),
-			charTwoLowerCase = charTwo.toLowerCase();
+		if (valueB === '{') {
+			return 1;
+		}
 
+		return -1;
+	}
 
-		function checkExceptions(valueA, valueB) {
-			if (valueA === '-') {
-				return 1;
-			}
-
-			if (valueB === '{') {
-				return 1;
-			}
-
+	if (charOneLowerCase < charTwoLowerCase) {
+		return checkExceptions(charOneLowerCase, charTwoLowerCase);
+	}
+	else if (charOneLowerCase > charTwoLowerCase) {
+		return 1;
+	}
+	else {
+		if (charOne < charTwo) {
 			return -1;
 		}
-
-		if (charOneLowerCase < charTwoLowerCase) {
-			return checkExceptions(charOneLowerCase, charTwoLowerCase);
-		}
-		else if (charOneLowerCase > charTwoLowerCase) {
+		else if (charOne > charTwo) {
 			return 1;
 		}
 		else {
-			if (charOne < charTwo) {
-				return -1;
-			}
-			else if (charOne > charTwo) {
-				return 1;
-			}
-			else {
-				return 0;
+			return 0;
+		}
+	}
+};
+
+sortLines = function (lines) {
+	for (const key in lines) {
+		const line = lines[key];
+
+		if (line.includes('import {')) {
+			const
+				rawImportedModules = line.match(/\{(.*)\}/)[1];
+
+			let importedModules = rawImportedModules.split(', ');
+
+			if (importedModules.length > 1) {
+				importedModules = importedModules.sort(superSort).join(', ');
+				lines[key] = lines[key].replace(rawImportedModules, importedModules);
 			}
 		}
-	});
+	}
+
+	lines.sort(superSort);
 
 	return lines;
 }
-
 
 console.log(sortLines(input));
